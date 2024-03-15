19b2df981d442566958e8c370e710a19c5dd3d41
Bogdan <bogdanion466@gmail.com>
2024-03-15 07:54:08 -0700

    first push

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS); // Be cautious with logging passwords
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_PORT);
+
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const port = 3003;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+app.get('/', async (req, res) => {
+
+
+    const { rows } = await pool.query('SELECT * FROM items ORDER BY RANDOM() LIMIT 1');
+    res.send(`<h1>Random Item: ${rows[0].content}</h1><button onclick="location.reload()">Next</button>`);
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
