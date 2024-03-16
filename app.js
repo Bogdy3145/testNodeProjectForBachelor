b8a1aa5d9d3a74ff70cf30152cc343c7f8166e50
Bogdan <bogdanion466@gmail.com>
2024-03-15 17:05:11 -0700

    doesnt matter

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ const app = express();
 const port = 3003;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
 });
 
 app.get('/', async (req, res) => {
